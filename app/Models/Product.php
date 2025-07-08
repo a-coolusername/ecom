f2cad669291f36ad1e334f05c3f4b58da79ccad9
a-coolusername <ahmadelasir@gmail.com>
2025-07-08 21:18:24 +0300

    establishing relationship many to one

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
 
 class Product extends Model
 {
-    //
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
