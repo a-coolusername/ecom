3a0b6f3ddcc18de0a4e105564b4231faeb624562
a-coolusername <ahmadelasir@gmail.com>
2025-07-08 11:51:24 +0300

    fixed grid for product view

--- resources/views/allProducts.blade.php
+++ resources/views/allProducts.blade.php
@@ -1,4 +1,35 @@
 @extends('layout')
-@section("content")
-<p>all products</p>
+@section('content')
+<div class='grid-container'>
+    <div><p>1</p></div>
+    <div><p>2</p></div>
+    <div><p>3</p></div>
+    <div><p>4</p></div>
+    <div><p>5</p></div>
+    <div><p>6</p></div>
+    <div><p>7</p></div>
+    <div><p>8</p></div>
+    <div><p>9</p></div>
+</div>
+@endsection
+@section('styles')
+<style>
+.grid-container {
+  display: grid;
+  grid-template-columns: auto auto auto;
+  padding: 20px;
+  margin-top: 30px;
+}
+.grid-container > div {
+  background-color: #f1f1f1;
+  border: 1px solid black;
+  padding: 10px;
+  font-size: 30px;
+  text-align: center;
+}
+.grid-container > div > p {
+  padding: 5px;
+  font-size: 30px;
+}
+</style>
 @endsection
