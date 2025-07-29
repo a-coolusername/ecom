8c470e5473d61cf3af12384ae559f8374d5434dc
a-coolusername <ahmadelasir@gmail.com>
2025-07-29 13:53:41 +0300

    JWT/APIs stage one, worked with postman etc.. AND created orders table -mc next, add to orders table

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,31 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use  Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
+
+
+    /**
+     * Get the identifier that will be stored in the JWT token.
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return an array with custom claims to be added to the JWT token.
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+
+
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasApiTokens;
 
--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,7 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
+use App\Http\Controllers\AuthController;
 
 // apiResource route expects to map to index, show, store, update, destroy, etc, im using home, all, clothes, etc.
 //Route::group(['prefix'=>'products'], function (){
@@ -16,16 +12,30 @@ Route::get('/user', function (Request $request) {
 
 Route::prefix('products')->controller(ProductController::class)->group(function () {
     Route::get('/', 'index');
-    Route::get('/all', 'all');
-    Route::get('/Home', 'home');
-    Route::get('/Electronics', 'electronics');
-    Route::get('/Pets', 'pets');
-    Route::get('/Games', 'games');
-    Route::get('/Clothes', 'clothes');
-    Route::post('/addToCart', 'addToCart');
-    Route::post('/remove-from-cart', 'removeFromCart');
-    Route::get('/cart', 'cart');
-    Route::get('/Checkout', 'checkout');
-    Route::get('/Purchase', 'purchase');
-    Route::get('/{id}', 'show');
+//    Route::get('/all', 'all');
+//    Route::get('/Home', 'home');
+//    Route::get('/Electronics', 'electronics');
+//    Route::get('/Pets', 'pets');
+//    Route::get('/Games', 'games');
+//    Route::get('/Clothes', 'clothes');
+//    Route::post('/addToCart', 'addToCart');
+//    Route::post('/remove-from-cart', 'removeFromCart');
+//    Route::get('/cart', 'cart');
+//    Route::get('/Checkout', 'checkout');
+//    Route::get('/Purchase', 'purchase');
+//    Route::get('/{id}', 'show');
 });
+
+Route::get('/', function () {
+    return response()->json(['message' => 'Hello world!']);
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('jwt')->group(function () {
+    Route::get('/user', [AuthController::class, 'getUser']);
+    Route::put('/user', [AuthController::class, 'updateUser']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
